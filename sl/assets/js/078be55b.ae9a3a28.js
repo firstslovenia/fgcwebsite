69392ce66626c1bef0d9a5a90376d18c7dabeb21
inightfulbriyan <32695570+insightfulbriyan@users.noreply.github.com>
2025-06-13 21:39:35 +0200

    Deploy website - based on 865ffb5664dd344e294025389a23e8b3744b6910

--- sl/assets/js/078be55b.ae9a3a28.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfgcwebsite=self.webpackChunkfgcwebsite||[]).push([[3913],{2609:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/sl/blog/page/4","page":4,"postsPerPage":10,"totalPages":4,"totalCount":32,"previousPage":"/sl/blog/page/3","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
